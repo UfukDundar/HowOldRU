98af62753e429ec743a2ba227ac8e9aef703ada7
ufuk <ufuk-dundar@outlook.com>
2016-12-28 19:07:20 +0300

    Updated

--- HowOldRU/Form1.Designer.cs
+++ HowOldRU/Form1.Designer.cs
@@ -28,11 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dtBirthday = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.txtAge = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTotalDay = new System.Windows.Forms.TextBox();
+            this.txtTotalHours = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTotalMinutes = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotalSeconds = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // dtBirthday
@@ -54,7 +64,7 @@
             // 
             // txtAge
             // 
-            this.txtAge.BackColor = System.Drawing.Color.Red;
+            this.txtAge.BackColor = System.Drawing.Color.White;
             this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txtAge.Location = new System.Drawing.Point(362, 78);
             this.txtAge.Name = "txtAge";
@@ -73,7 +83,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(129, 106);
+            this.button1.Location = new System.Drawing.Point(554, 78);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(227, 33);
             this.button1.TabIndex = 4;
@@ -81,11 +91,92 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(149, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Total Day";
+            // 
+            // txtTotalDay
+            // 
+            this.txtTotalDay.Location = new System.Drawing.Point(257, 196);
+            this.txtTotalDay.Name = "txtTotalDay";
+            this.txtTotalDay.Size = new System.Drawing.Size(181, 22);
+            this.txtTotalDay.TabIndex = 6;
+            // 
+            // txtTotalHours
+            // 
+            this.txtTotalHours.Location = new System.Drawing.Point(257, 238);
+            this.txtTotalHours.Name = "txtTotalHours";
+            this.txtTotalHours.Size = new System.Drawing.Size(181, 22);
+            this.txtTotalHours.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(149, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Total Hours";
+            // 
+            // txtTotalMinutes
+            // 
+            this.txtTotalMinutes.Location = new System.Drawing.Point(257, 285);
+            this.txtTotalMinutes.Name = "txtTotalMinutes";
+            this.txtTotalMinutes.Size = new System.Drawing.Size(181, 22);
+            this.txtTotalMinutes.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(149, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Total Minutes";
+            // 
+            // txtTotalSeconds
+            // 
+            this.txtTotalSeconds.Location = new System.Drawing.Point(257, 324);
+            this.txtTotalSeconds.Name = "txtTotalSeconds";
+            this.txtTotalSeconds.Size = new System.Drawing.Size(181, 22);
+            this.txtTotalSeconds.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(149, 329);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Total Seconds";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(553, 251);
+            this.ClientSize = new System.Drawing.Size(817, 374);
+            this.Controls.Add(this.txtTotalSeconds);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTotalMinutes);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTotalHours);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTotalDay);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtAge);
@@ -105,6 +196,15 @@
         private System.Windows.Forms.TextBox txtAge;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTotalDay;
+        private System.Windows.Forms.TextBox txtTotalHours;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTotalMinutes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotalSeconds;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- HowOldRU/Form1.cs
+++ HowOldRU/Form1.cs
@@ -23,6 +23,23 @@ namespace HowOldRU
             DateTime dtPresent = DateTime.Now;
             int age = dtPresent.Year - dtBirth.Year;
             txtAge.Text = age.ToString();
+            txtTotalDay.Text = (dtPresent - dtBirth).TotalDays.ToString();
+            txtTotalHours.Text = (dtPresent - dtBirth).TotalHours.ToString();
+            txtTotalMinutes.Text = (dtPresent - dtBirth).TotalMinutes.ToString();
+            txtTotalSeconds.Text = (dtPresent - dtBirth).TotalSeconds.ToString();
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dtBirth = dtBirthday.Value;
+            DateTime dtPresent = DateTime.Now;
+            int age = dtPresent.Year - dtBirth.Year;
+            txtAge.Text = age.ToString();
+            txtTotalDay.Text = (dtPresent - dtBirth).TotalDays.ToString("#,###");
+            txtTotalHours.Text = (dtPresent - dtBirth).TotalHours.ToString("#,###");
+            txtTotalMinutes.Text = (dtPresent - dtBirth).TotalMinutes.ToString("#,###");
+            txtTotalSeconds.Text = (dtPresent - dtBirth).TotalSeconds.ToString("#,###");
         }
     }
 }
